21ad1946ce2071baed4b9f951c2ebe5b1f05b769
Ned Henry <ethenry@ucsc.edu>
2017-02-17 10:52:11 -0800

    re-indented import form

--- forms/ImportForm.php
+++ forms/ImportForm.php
@@ -31,54 +31,54 @@ class Vimeo_Form_Import extends Omeka_Form
      */
     private function _registerElements()
     {
-      //hashed anti-csrf nonce:
-      //$this->addElement('hash', 'vimeoNonce', array('salt'=>'oilyravinecakes'));
+        //hashed anti-csrf nonce:
+        //$this->addElement('hash', 'vimeoNonce', array('salt'=>'oilyravinecakes'));
 
-      //URL:
-      $this->addElement('text', 'vimeourl', array(
-						    'label'         => __('Vimeo URL'),
-						    'description'   => __('Paste the full url of the Vimeo video you would like to import'),
-						    'validators'    =>array(
-                                array('callback',false,array('callback'=>array($this,'validateVimeoUrl'),'options'=>array()))
-                            ),
-						    'order'         => 1,
-						    'required'      => true,
-                                                    'title'         => 'Copy the URL from your browser’s URL bar or the YouTube “share” link'
-						    )
-			);
+        //URL:
+        $this->addElement('text', 'vimeourl', array(
+	    'label'         => __('Vimeo URL'),
+	    'description'   => __('Paste the full url of the Vimeo video you would like to import'),
+	    'validators'    =>array(
+                array('callback',false,array('callback'=>array($this,'validateVimeoUrl'),'options'=>array()))
+            ),
+	    'order'         => 1,
+	    'required'      => true,
+            'title'         => 'Copy the URL from your browser’s URL bar or the YouTube “share” link'
+	)
+	);
 
 	// Collection:
         $this->addElement('select', 'vimeocollection', array(
-							'label'         => __('Collection'),
-							'description'   => __('Select a collection'),
-							'value'         => '0',
-							'order'         => 2,
-							'multiOptions'       => $this->_getCollectionOptions()
-							)
-			  );
+	    'label'         => __('Collection'),
+	    'description'   => __('Select a collection'),
+	    'value'         => '0',
+	    'order'         => 2,
+	    'multiOptions'       => $this->_getCollectionOptions()
+	)
+	);
 
 	// Responsibility (User Role):
         $this->addElement('select', 'vimeouserrole', array(
-								'label'         => __('Responsibility'),
-								'description'   => __('The Vimeo user is the ______ of the video'),
-								'value'         => 'Publisher',
-								'order'         => 3,
-								
-								'multiOptions'       => $this->_getRoleOptions(),
+	    'label'         => __('Responsibility'),
+	    'description'   => __('The Vimeo user is the ______ of the video'),
+	    'value'         => 'Publisher',
+	    'order'         => 3,
+	    
+	    'multiOptions'       => $this->_getRoleOptions(),
             'title' => 'This will determine the Dublin Core field in which the YouTube user/channel name will appear.'
-								)
-			  );
+	)
+	);
 
 
-     
+        
         // Visibility (public vs private):
         $this->addElement('checkbox', 'vimeopublic', array(
             'label'         => __('Public Visibility'),
             'description'   => __('Would you like to make the video public in Omeka?'),
             'checked'         => 'checked',
 	    'order'         => 4
-							   )
-			  );
+	)
+	);
 
 	$this->addElement('hash','vimeo_token');
 
@@ -89,21 +89,21 @@ class Vimeo_Form_Import extends Omeka_Form
 
 	//Display Groups:
         $this->addDisplayGroup(
-			       array(
-				     'vimeourl',
-				     'vimeocollection',
-				     'vimeouserrole',
-				     'vimeopublic'
-				     ),
-			       'fields'
-			       );
+	    array(
+		'vimeourl',
+		'vimeocollection',
+		'vimeouserrole',
+		'vimeopublic'
+	    ),
+	    'fields'
+	);
 
         $this->addDisplayGroup(
-			       array(
-				     'vimeo-import-submit'
-				     ), 
-			       'submit_buttons'
-			       );
+	    array(
+		'vimeo-import-submit'
+	    ), 
+	    'submit_buttons'
+	);
 
     }
 
@@ -114,78 +114,78 @@ class Vimeo_Form_Import extends Omeka_Form
      */
     public static function ProcessPost()
     {
-      $_REQUEST['vimeourl'] = self::_resolveShortUrl($_REQUEST['vimeourl']);
-      try {
-          if(self::_importSingle())
-	      return('Go to Items or Collections to view imported videos.');
-      } catch(Exception $e) {
-          throw new Exception('Error importing video. '.$e->getMessage());
-      }
-      return(true);
+        $_REQUEST['vimeourl'] = self::_resolveShortUrl($_REQUEST['vimeourl']);
+        try {
+            if(self::_importSingle())
+	        return('Go to Items or Collections to view imported videos.');
+        } catch(Exception $e) {
+            throw new Exception('Error importing video. '.$e->getMessage());
+        }
+        return(true);
     }
 
 
     /**
-   * Import a single video in real time (not in the background).
-   *
-   * This function relies on the import form output being in the
-   * $_POST variable. The form should be validated before calling this.
-   *
-   * @return bool $success true if no error, false otherwise
-   */
-  private static function _importSingle()
+     * Import a single video in real time (not in the background).
+     *
+     * This function relies on the import form output being in the
+     * $_POST variable. The form should be validated before calling this.
+     *
+     * @return bool $success true if no error, false otherwise
+     */
+    private static function _importSingle()
     {
-      require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'import.php';
+        require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'import.php';
 
-      $public = isset($_REQUEST['vimeopublic']) ? $_REQUEST['vimeopublic'] : false; 
+        $public = isset($_REQUEST['vimeopublic']) ? $_REQUEST['vimeopublic'] : false; 
 
-      try{
-          $videoID = VimeoImport_ImportHelper::ParseURL($_REQUEST['vimeourl']);
-          $response =  VimeoImport_ImportHelper::GetVideo(
-              $videoID,
-              $_REQUEST['vimeocollection'],
-              $_REQUEST['vimeouserrole'],
-              $_REQUEST['vimeopublic']
-          );
+        try{
+            $videoID = VimeoImport_ImportHelper::ParseURL($_REQUEST['vimeourl']);
+            $response =  VimeoImport_ImportHelper::GetVideo(
+                $videoID,
+                $_REQUEST['vimeocollection'],
+                $_REQUEST['vimeouserrole'],
+                $_REQUEST['vimeopublic']
+            );
 
-          $post = $response['post'];
-          $files = $response['files'];
-      } catch (Exception $e) {
-          throw $e;
-      }
+            $post = $response['post'];
+            $files = $response['files'];
+        } catch (Exception $e) {
+            throw $e;
+        }
 
-      $record = new Item();
-      
-      $record->setPostData($post);
-   
-      if (!$record->save(false)) {
-          throw new Exception($record->getErrors());
-      }
-      
-      if(!empty($files)&&!empty($record))
-          {
-              if(!insert_files_for_item($record,'Url',$files))
-                  throw new Exception("Error attaching files");
-          }
-      return(true);
+        $record = new Item();
+        
+        $record->setPostData($post);
+        
+        if (!$record->save(false)) {
+            throw new Exception($record->getErrors());
+        }
+        
+        if(!empty($files)&&!empty($record))
+        {
+            if(!insert_files_for_item($record,'Url',$files))
+                throw new Exception("Error attaching files");
+        }
+        return(true);
     }
 
-  /**
-   * Get an array to be used in formSelect() containing possible user roles.
-   * 
-   * @return array $options An associative array mapping dublin core elements
-   * which could be associated with the Vimeo usernames, to their display 
-   * values in a dropdown menu.
-   */
+    /**
+     * Get an array to be used in formSelect() containing possible user roles.
+     * 
+     * @return array $options An associative array mapping dublin core elements
+     * which could be associated with the Vimeo usernames, to their display 
+     * values in a dropdown menu.
+     */
     private function _getRoleOptions()
     {
-      $options = array(
-		     '0'=>'No Role',
-		     'Contributor'=>'Contributor',
-		     'Creator'=>'Creator',
-		     'Publisher'=>'Publisher'
-		     );
-    return $options;
+        $options = array(
+	    '0'=>'No Role',
+	    'Contributor'=>'Contributor',
+	    'Creator'=>'Creator',
+	    'Publisher'=>'Publisher'
+	);
+        return $options;
     }
 
     /**
@@ -202,7 +202,7 @@ class Vimeo_Form_Import extends Omeka_Form
                 $element->setDecorators(array(
                     'ViewHelper', 
                     array('HtmlTag', array('tag' => 'div'))
-					      ));
+		));
             } else if($element instanceof Zend_Form_Element_Radio) {
                 // Radio buttons must have a 'radio' class on the div wrapper.
                 $element->getDecorator('InputsTag')->setOption('class', 'inputs radio five columns alpha');
@@ -216,61 +216,61 @@ class Vimeo_Form_Import extends Omeka_Form
     }
 
 
-  /**
-   * Get an array to be used in formSelect() containing all collections.
-   * 
-   * @return array $options An associative array mapping collection IDs
-   *to their titles for display in a dropdown menu
-   */
+    /**
+     * Get an array to be used in formSelect() containing all collections.
+     * 
+     * @return array $options An associative array mapping collection IDs
+     *to their titles for display in a dropdown menu
+     */
     private function _getCollectionOptions()
     {
-      $collections = get_records('Collection',array(),'0');
-      $options = array('0'=>'Assign No Collection');
-      foreach ($collections as $collection)
+        $collections = get_records('Collection',array(),'0');
+        $options = array('0'=>'Assign No Collection');
+        foreach ($collections as $collection)
 	{
-	  $titles = $collection->getElementTexts('Dublin Core','Title');
-	  if(isset($titles[0]))
-	    $title = $titles[0];
-	  $options[$collection->id]=$title;
+	    $titles = $collection->getElementTexts('Dublin Core','Title');
+	    if(isset($titles[0]))
+	        $title = $titles[0];
+	    $options[$collection->id]=$title;
 	}
 
-      return $options;
+        return $options;
     }
 
-  /**
-   * Resolve a shortened URL and return the full url
-   * 
-   * @param string $shortUrl The shortened Flickr url of the photo to import.
-   * @return string $fullUrl The full Flickr url of the photo to import.
-   */
-  private static function _resolveShortUrl($shortUrl)
-  {
-    //if the url contains 'vimeo.com', it's not a short url. just return it.
-    if(strpos($shortUrl,'vimeo.com'))
-      return($shortUrl);
+    /**
+     * Resolve a shortened URL and return the full url
+     * 
+     * @param string $shortUrl The shortened Flickr url of the photo to import.
+     * @return string $fullUrl The full Flickr url of the photo to import.
+     */
+    private static function _resolveShortUrl($shortUrl)
+    {
+        //if the url contains 'vimeo.com', it's not a short url. just return it.
+        if(strpos($shortUrl,'vimeo.com'))
+            return($shortUrl);
 
-    $fullUrl = self::_resolveRedirect($shortUrl);
-    return $fullUrl;
-  }
-
-/**
-   * Resolve a redirect and return the redirected url
-   * 
-   * @param string $url The url which is redirected.
-   * @return string $fullUrl The destination url.
-   */
-  private static function _resolveRedirect($url)
-  {
-    $headers = get_headers($url);
-    $headers = array_reverse($headers);
-    foreach($headers as $header) {
-      if (strpos($header, 'Location: ') === 0 ) {
-	$fullUrl = "https://flickr.com".str_replace('Location: ', '', $header);
-	break;
-      }
+        $fullUrl = self::_resolveRedirect($shortUrl);
+        return $fullUrl;
     }
-    return $fullUrl;
-  }
+
+    /**
+     * Resolve a redirect and return the redirected url
+     * 
+     * @param string $url The url which is redirected.
+     * @return string $fullUrl The destination url.
+     */
+    private static function _resolveRedirect($url)
+    {
+        $headers = get_headers($url);
+        $headers = array_reverse($headers);
+        foreach($headers as $header) {
+            if (strpos($header, 'Location: ') === 0 ) {
+	        $fullUrl = "https://flickr.com".str_replace('Location: ', '', $header);
+	        break;
+            }
+        }
+        return $fullUrl;
+    }
 
     /**
      * Validate the vimeo url
@@ -280,9 +280,9 @@ class Vimeo_Form_Import extends Omeka_Form
      *@return bool $valid Indicates whether this url points to a valid vimeo
      */
     public function validateVimeoUrl($url,$args){
-      if(!strpos($url,'vimeo.com'))
-          return false;
-      return true;
+        if(!strpos($url,'vimeo.com'))
+            return false;
+        return true;
     }
 
 
